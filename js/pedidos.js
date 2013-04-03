d3b047e7ece629dc3822339dbf0e940554520023
Lucas Pinheiro <pinheirolouco@gmail.com>
2013-04-03 10:28:14 -0300

    versao 03/04/2013 i

--- js/pedidos.js
+++ js/pedidos.js
@@ -3,15 +3,9 @@ $(document).on('pageinit', function() {
     $('table#table-consulta-pedidos tbody tr').each(function() {
         i++;
         if (i > 5) {
-            i = 0;
+            i = 1;
         }
         $(this).find("td:eq(3)").html('<b class="ui-table-cell-label">Situação</b><span class="situacoes_pedido_' + i + '">' + _situacoes.pedido[i] + '</span>');
-    });
-    $.each(_situacoes.sincronizacao, function(a, b) {
-        $('#situacao_envio').append('<option value="' + a + '">' + b + '</option>');
-    });
-    $.each(_situacoes.pedido, function(a, b) {
-        $('#situacao_pedido').append('<option value="' + a + '">' + b + '</option>');
     });
     $('form').insere_mascara();
 });
